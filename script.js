e9f862f3ac568a03a0a6e9c3eadbd38973b27adb
sofiamorschelneves-cpu <sofia.morschel.neves@escola.pr.gov.br>
2026-07-02 07:54:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -134,3 +134,8 @@ function inicializarContatoModerno() {
         });
     });
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    inicializarQuizModerno();
+    inicializarContatoModerno();
+});
